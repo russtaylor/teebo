c8a42d8233824f35a0a0a06f31c57d3367f59393
Russ Taylor <russ@russt.me>
2014-09-10 22:57:45 -0700

    Making some progress toward timing a count of names from yaml.

--- lib/teebo.rb
+++ lib/teebo.rb
@@ -1,5 +1,18 @@
+require 'yaml'
+
 class Teebo
-  def self.initial
-    puts "Hello, world!"
+
+  def initialize
+    # Load YAML for hardcoded data
+    @en = YAML::load_file(File.join(__dir__, 'locales', 'en.yml'))
   end
+
+  def sum_count
+    sum = 0
+    for element in @en.names.given
+      sum += element.count
+    end
+    puts sum
+  end
+
 end
