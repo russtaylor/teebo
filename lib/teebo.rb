4d574fc9de8c4f5f3f82e1a2e685038d62c9cf17
Russ Taylor <russ@russt.me>
2014-09-11 23:11:05 -0700

    I think using the database will be better.

--- lib/teebo.rb
+++ lib/teebo.rb
@@ -4,8 +4,8 @@ require 'sqlite3'
 class Teebo
 
   def initialize
-    # Load YAML for hardcoded data
-    @en = YAML::load_file(File.join(__dir__, 'locales', 'en.yml'))
+    # Initialize the database
+    @database = SQLite3::Database.new "lib/data/seed-data.db"
   end
 
   def weight
@@ -13,11 +13,10 @@ class Teebo
   end
 
   def sum_count
-    sum = 0
-    @en['names']['given'].each do |element|
-      sum += element['count']
-    end
-    puts sum
+    count = @database.execute <<-SQL
+      select sum(count) from 'given_names' where sex = 'F'
+    SQL
+    print count
   end
 
 end
