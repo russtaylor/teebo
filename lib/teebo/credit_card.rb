379e7120f3f25d10a0404e25e7c834b8a173753b
Russ Taylor <russ@russt.me>
2014-09-21 22:11:44 -0700

    Beginnings of credit card class.

---
+++ lib/teebo/credit_card.rb
@@ -0,0 +1,10 @@
+module Teebo
+  #
+  # Helper class for generating credit card numbers. Categorizes cards according to their card
+  # issuer, while attempting to maintain a realistic distribution between the issuers. Also ensures
+  # that the credit card numbers are actually valid.
+  #
+  class CreditCard < TeeboGenerator
+    
+  end
+end
