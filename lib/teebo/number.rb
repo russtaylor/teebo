177cbae714d8107640c68b901e55cb2298db5c64
Russ Taylor <russ@russt.me>
2014-09-27 23:03:34 -0700

    Removing Gaussian class in favor of a simpler implementation.

--- lib/teebo/number.rb
+++ lib/teebo/number.rb
@@ -1,11 +1,19 @@
 module Teebo
   #
-  # Helper for creating numbers according to various distributions.
+  # Helper for generating  numbers according to various distributions.
   #
   class Number
-
-    def normal_dist(center, std_deviation)
-
+    #
+    # Generates a number using a normal distribution.
+    #
+    # A basic implementation of the Box-Muller transform. Adapted from an answer by antonakos on
+    # Stack Overflow here: http://stackoverflow.com/questions/5825680
+    #
+    def normal_dist(mean, std_deviation, rand = lambda { Kernel.rand })
+      theta = 2 * Math::PI * rand.call
+      rho = Math.sqrt(-2 * Math.log(1 - rand.call))
+      scale = std_deviation * rho
+      mean + scale * Math.cos(theta)
     end
 
   end
